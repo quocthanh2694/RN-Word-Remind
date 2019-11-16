b587960c9fc7ebb78dcaac0fee14b758a72da5b7
Thanh Tran <thanh.tran@loyaworld.com>
2019-11-16 14:28:34 +0700

    add React Native DateTimePicker

--- android/app/src/main/java/com/reactnativevocab/MainApplication.java
+++ android/app/src/main/java/com/reactnativevocab/MainApplication.java
@@ -11,7 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 // import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;  // <--- Import Package
-
+// import com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -28,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new ReactNativePushNotificationPackage());
+          // packages.add(new RNDateTimePickerPackage());
           return packages;
         }
 
